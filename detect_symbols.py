09a05dfa403b833218d840cbe805020de85301aa
Anass Kaziz <anass.kaziz@ev-box.com>
2021-03-14 20:28:07 +0100

    Added add_borders function

--- detect_symbols.py
+++ detect_symbols.py
@@ -8,6 +8,7 @@ class SymbolDetector:
         self.resized_image = None
         self.dilated_image = None
         self.thresholded = None
+        self.cropped_image = None
         self.kernel_size = kernel_size
         self.ratio = None
         self.contours = None
@@ -62,6 +63,27 @@ class SymbolDetector:
         self.compute_contour_centers()
         self.display_contours_on_image()
 
+    @staticmethod
+    def add_borders(desired_size, image):
+        image_path = image
+        im = cv2.imread(image_path)
+        old_size = im.shape[:2]  # old_size is in (height, width) format
+        ratio = float(desired_size) / max(old_size)
+        new_size = tuple([int(x * ratio) for x in old_size])  # new_size should be in (width, height) format
+
+        # resize the image
+        im = cv2.resize(im, (new_size[1], new_size[0]))
+        delta_w = desired_size - new_size[1]
+        delta_h = desired_size - new_size[0]
+        top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+        left, right = delta_w // 2, delta_w - (delta_w // 2)
+        color = [255, 255, 255]  # White
+        new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,
+                                    value=color)
+        # Show the image
+        cv2.imshow("new_im", new_im)
+        cv2.imwrite("resized.png", new_im)
+
     def display_contours_on_image(self):
         read_image = cv2.imread(self.image)
         for k in range(0, len(self.coordinates_list)):
@@ -72,50 +94,19 @@ class SymbolDetector:
             c = c.astype("float")
             c *= self.ratio  # the counter is smaller if it is not multiplied by the ratio
             c = c.astype("int")
+
+            # Crop the image
             x, y, w, h = cv2.boundingRect(c)
+            self.cropped_image = read_image[y-2:y + h+2, x-2:x + w+2]
+
             # cv2.rectangle(read_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # cv2.drawContours(read_image, [c], -1, (0, 255, 0), 2)
 
-            # Crop the image
-            cropped_image = read_image[y-2:y + h+2, x-2:x + w+2]
-
             # Save the image
-            image_name = f'cropped_{k}.jpg'
-            cv2.imwrite(image_name, cropped_image)
-
-            desired_size = 50
-            im_pth = image_name
-
-            im = cv2.imread(im_pth)
-            old_size = im.shape[:2]  # old_size is in (height, width) format
-
-            ratio = float(desired_size) / max(old_size)
-            new_size = tuple([int(x * ratio) for x in old_size])
-
-            # new_size should be in (width, height) format
-
-            im = cv2.resize(im, (new_size[1], new_size[0]))
-
-            delta_w = desired_size - new_size[1]
-            delta_h = desired_size - new_size[0]
-            top, bottom = delta_h // 2, delta_h - (delta_h // 2)
-            left, right = delta_w // 2, delta_w - (delta_w // 2)
-
-            color = [255, 255, 255]
-            new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,
-                                        value=color)
-
-            # Show the image
-            cv2.imshow("new_im", new_im)
-            cv2.imwrite("resized.png", new_im)
+            cropped_image_name = f'cropped_{k}.jpg'
+            cv2.imwrite(cropped_image_name, self.cropped_image)
+            self.add_borders(desired_size=50, image=cropped_image_name)
 
             # show the output image
             cv2.imshow("Image", read_image)
             cv2.waitKey(0)
-
-
-# image = cv2.imread("formula.png")
-# sd = SymbolDetector(image="formula.png")
-# sd.compute_contour_centers()
-# sd.display_contours_on_image()
-
